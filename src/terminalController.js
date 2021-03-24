a8fc6ee56c3eea5f3784f2394a3ec90add514d91
Wallace Belem Teixeira <wllcblm@gmail.com>
2021-03-24 00:15:59 -0300

    cli interface finished

---
+++ src/terminalController.js
@@ -0,0 +1,105 @@
+import ComponentsBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+    #usersColors = new Map();
+
+    constructor() {}
+
+    #pickColor() {
+        return `#${(((1 << 24) * Math.random()) | 0).toString(16)}-fg`;
+    }
+
+    #getUserColor(userName) {
+        if (this.#usersColors.has(userName))
+            return this.#usersColors.get(userName);
+
+        const color = this.#pickColor();
+        this.#usersColors.set(userName, color);
+
+        return color;
+    }
+
+    #onInputReceived(eventEmitter) {
+        return function () {
+            const message = this.getValue();
+            console.log(message);
+            this.clearValue();
+        };
+    }
+
+    #onMessageReceived({ screen, chat }) {
+        return (msg) => {
+            const { userName, message } = msg;
+            const color = this.#getUserColor(userName);
+
+            chat.addItem(`{${color}}{bold}${userName}:{/}${message}`);
+            screen.render();
+        };
+    }
+
+    #onLogChanged({ screen, activityLog }) {
+        return (msg) => {
+            const [userName] = msg.split(/\s/);
+            const color = this.#getUserColor(userName);
+            activityLog.addItem(`{${color}}{bold}${msg.toString()}{/}`);
+
+            screen.render();
+        };
+    }
+
+    #onStatusChanged({ screen, status }) {
+        return (users) => {
+            const { content } = status.items.shift();
+            status.clearItems();
+            status.addItem(content);
+
+            users.forEach((userName) => {
+                const color = this.#getUserColor(userName);
+                status.addItem(`{${color}}{bold}${userName}{/}`);
+            });
+
+            screen.render();
+        };
+    }
+
+    #registerEvents(eventEmitter, components) {
+        eventEmitter.on(
+            constants.events.app.MESSAGE_RECEIVED,
+            this.#onMessageReceived(components)
+        );
+
+        eventEmitter.on(
+            constants.events.app.ACTIVITYLOG_UPDATED,
+            this.#onLogChanged(components)
+        );
+
+        eventEmitter.on(
+            constants.events.app.STATUS_UPDATED,
+            this.#onStatusChanged(components)
+        );
+    }
+
+    async initializeTable(eventEmitter) {
+        const components = new ComponentsBuilder()
+            .setScreen({ title: "HackerChat - Wallace" })
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived)
+            .setChatComponent()
+            .setActivityLogComponent()
+            .setStatusComponent()
+            .build();
+
+        this.#registerEvents(eventEmitter, components);
+
+        components.input.focus();
+        components.screen.render();
+
+        const users = ["txrwall"];
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push("mobilia_nova-02");
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push("captainK1nd3r", "ded@0_de_4ssO");
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+    }
+}
